743a181d87dff36c5012b42edc017d8e51d5dbb1
honza <jan.kubant@blindspot.ai>
2020-05-13 08:48:20 +0200

    forgotten files

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.7.7-buster
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "app.py" ]
+
+EXPOSE ${PORT}
---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+
+from cathegory_statistics.stats import update_cathegory_statistics
+from configuration.config import load_conf
+from model.model import fit_model_and_create_predictions
+from prediction.prediction import prediction_for_user
+
+app = Flask(__name__)
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    fit_model_and_create_predictions()
+    update_cathegory_statistics()
+
+
+@app.route('/<usr_id>', methods=['GET'])
+def predict(usr_id):
+    return prediction_for_user(usr_id)
+
+
+if __name__ == '__main__':
+    load_conf(app)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT"))
